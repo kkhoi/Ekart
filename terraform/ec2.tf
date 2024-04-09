055a15b19ac096994f7235e938a8c5a115058e3d
khoi <ngminkhoi@gmail.com>
2024-04-09 10:20:11 +0700

    add terraform file

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "jenkins" {
-  ami = data.aws_ami.amzlinux2.id
+  ami = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-  user_data = file("${path.module}/install_jenkins.sh")
+  # user_data = file("${path.module}/install_jenkins.sh")
   key_name = var.instance_keypair
   vpc_security_group_ids = [ aws_security_group.vpc-app.id]
   tags = {
