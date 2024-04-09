ee26648f8fe4078d6dd4ca81072d90c0bdbd56b9
kkhoi <107262078+kkhoi@users.noreply.github.com>
2024-04-09 10:02:47 +0700

    Create ec2.tf

---
+++ terraform/ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "jenkins" {
+  ami = data.aws_ami.amzlinux2.id
+  instance_type = var.instance_type
+  user_data = file("${path.module}/install_jenkins.sh")
+  key_name = var.instance_keypair
+  vpc_security_group_ids = [ aws_security_group.vpc-app.id]
+  tags = {
+    "Name" = "Jenkins server"
+  }
+}
