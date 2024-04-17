4f14723022bbde17f3c5d6ef2b756ee4433c8c4e
kkhoi <107262078+kkhoi@users.noreply.github.com>
2024-04-17 12:10:55 +0700

    Create intall_k8s_master.sh

---
+++ terraform/k8s-master/intall_k8s_master.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+apt update
+apt  install docker.io -y
+service docker restart
+cd /etc/apt/
+mkdir keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt update
+sudo apt install -y kubeadm=1.28.1-1.1 kubelet=1.28.1-1.1 kubectl=1.28.1-1.1
+sudo kubeadm init --pod-network-cidr=192.168.0.0/16
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.49.0/deploy/static/provider/baremetal/deploy.yaml
